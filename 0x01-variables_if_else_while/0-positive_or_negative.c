849f7753d8ebf4d314209fefda594bda52cdab57
Inno365 <androidvr6@gmail.com>
2023-03-17 18:03:17 -0700

    blank lin fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*  main - returns a zero integer 
-*  checks and prints numbers 
+*main - returns a zero integer 
+*checks and prints numbers 
 */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
