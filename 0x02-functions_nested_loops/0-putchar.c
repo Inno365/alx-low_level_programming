01b3fba211871cc01a5dcb5ce8243948311ad896
Inno365 <androidvr6@gmail.com>
2023-03-21 19:12:58 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_alphabet - start parsing from this main function.
+ * Return: 0 on exit, always.
+ */
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
+}
