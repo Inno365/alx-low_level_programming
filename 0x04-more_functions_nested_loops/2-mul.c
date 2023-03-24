e9e3db0fa247a65908204287fa2925979886ea96
Inno365 <androidvr6@gmail.com>
2023-03-23 18:38:19 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer to multiplied to b
+ * @b: second int to multiplied to a
+ * Return: returns product of int a and b
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+
+	return (c);
+}
