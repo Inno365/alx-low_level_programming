f92442e075d6dc10fc02b88cf497b6f0213b1bfa
Inno365 <androidvr6@gmail.com>
2023-03-23 18:32:27 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - Method that checks for a digits (from 0 to 9)
+ * @c: To checked
+ * Return: will return 1 if c is a digit else return 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
