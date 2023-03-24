1caeaaa6ff01925f9091d5d58c5688f7e73e14da
Inno365 <androidvr6@gmail.com>
2023-03-23 18:26:44 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - mrthod that checks for uppercase character
+ * @c: To checked
+ * Return: will return 1 if c is uppercase else return 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
