123cec8d40c114e734860cd11d9297acfb901f41
Inno365 <androidvr6@gmail.com>
2023-03-27 17:05:25 -0700

    fix betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
  * swap_int - swaps the values of two integers.
  * @a: pointer to first value.
  * @b: pointer to second value.
- * @s: variable to hold swaped value.
  */
 void swap_int(int *a, int *b)
 {
