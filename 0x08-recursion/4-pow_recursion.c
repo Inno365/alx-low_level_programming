808160ec27afe75f62ec92daf189b991c738b7ae
Inno365 <androidvr6@gmail.com>
2023-04-05 16:29:12 -0700

    raised to power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_pow_recursion - returns value of x raised by power of y.
+ *@y: is a power.
+ *@x: is a base.
+ *Return: base raise to power.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		{
+			return (1);
+		}
+	if (y < 0)
+		{
+		return (-1);
+		}
+	return (x * _pow_recursion(x, y - 1));
+}
