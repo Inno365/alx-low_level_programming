74d2176670190b2e93d263418842ac3f30a6ad03
Inno365 <androidvr6@gmail.com>
2023-04-05 15:38:49 -0700

    fix betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _print_rv_recursion - reverses the printed string.
+ * _print_rev_recursion - reverses the printed string.
  * @s: string to print.
  */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
