732106a517d2e52751543d80f36c4c5f664a8b6d
Inno365 <androidvr6@gmail.com>
2023-04-05 17:23:44 -0700

    prime_numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,19 +2,16 @@
 #include <math.h>
 
 int is_prime_number(int n) {
-    // Handle special cases
     if (n <= 1) {
         return 0;
     } else if (n <= 3) {
         return 1;
     }
 
-    // Check if n is divisible by 2 or 3
     if (n % 2 == 0 || n % 3 == 0) {
         return 0;
     }
 
-    // Check if n is divisible by any number up to its square root
     int i;
     for (i = 5; i <= sqrt(n); i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) {
@@ -22,6 +19,5 @@ int is_prime_number(int n) {
         }
     }
 
-    // If n is not divisible by any number up to its square root, it is prime
     return 1;
 }
