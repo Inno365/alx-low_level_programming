53e6dff1f03d84224e35ae5c1f5d3e7cd62fa6a2
Inno365 <androidvr6@gmail.com>
2023-04-05 17:43:20 -0700

    primes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,32 @@
 #include "main.h"
-#include <math.h>
 
-int is_prime_number(int n) {
-    if (n <= 1) {
-        return 0;
-    } else if (n <= 3) {
-        return 1;
-    }
+int actual_prime(int n, int i);
 
-    if (n % 2 == 0 || n % 3 == 0) {
-        return 0;
-    }
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
 
-    int i;
-    for (i = 5; i <= sqrt(n); i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) {
-            return 0;
-        }
-    }
-
-    return 1;
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
