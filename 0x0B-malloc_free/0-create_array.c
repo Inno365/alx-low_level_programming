d18049e25e3d81b060e7073a9e0ce69886bdd5fa
Inno365 <androidvr6@gmail.com>
2023-04-11 17:44:11 -0700

    fix betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *creates an array of char with specific char initialization.
+ *create_array - creates an array of char with specific char initialization.
  *@c: start an array of char.
  *@size: allocate size of array.
  *Return: pointer (success), NULL (Error).
@@ -16,15 +16,20 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	{
 		return (NULL);
+	}
+
 	p = (char *) malloc(sizeof(char) * size);
+
 	if (p == NULL)
 	{
 		return (0);
 	}
+
 	while (i < size)
 	{
 		*(p + i) = c;
 		i++;
 	}
-	}
+	*(p + i) = '\0';
+	return (p);
 }
