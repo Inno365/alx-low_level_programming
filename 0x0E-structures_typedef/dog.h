db4a58c0c9888fb77fbee86db1adfe2dd20ee5cb
Inno365 <androidvr6@gmail.com>
2023-04-17 20:28:39 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ *struct dog - dog details.
+ *@name: name of dog.
+ *@age: age of dog.
+ *@owner: owner of dog.
+ */
+ struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
